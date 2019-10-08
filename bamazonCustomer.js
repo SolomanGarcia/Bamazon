af33cdf457e23ad966826784f67b37290cd1d80b
Soloman Garcia <soloman.garcia@gmail.com>
2019-10-08 09:35:58 -0600

    working on bamazonCustomer js

---
+++ bamazonCustomer.js
@@ -0,0 +1,26 @@
+//Require mysql and inquirer
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+//Create a connection to database
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Vanilla1",
+    database: "bamazon_db"
+  })
+
+  function start() {
+    connection.query("SELECT * FROM Products", function(err, res){
+        if(err) throw err;
+
+        console.log("----------Welcome to Bamazon----------")
+        console.log("----------------------------------------------------------------------------------------------------")
+  
+        for( var i = 0; i < res.lenght; i++) {
+            console.log("Id " + res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quanity);
+            console.log("----------------------------------------------------------------------------------------------------")
+        }
+    })
+  }
