c06b15f589c4539c32c5b2d2f3abaa5c5e174f02
Soloman Garcia <soloman.garcia@gmail.com>
2019-10-09 09:31:19 -0600

    debugging

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 //Require mysql and inquirer
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var consoleTable = require("console.table");
 var pw = require("./pw.js");
 //Create a connection to mysql
 var connection = mysql.createConnection({
@@ -9,17 +10,17 @@ var connection = mysql.createConnection({
     user: "root",
     password: pw.pw,
     database: "bamazon_db"
-  });
+});
 //Create a connection to database
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log("\n--------------------Welocome to Bamazon--------------------\n")
-//Start
+    //Start
     start();
 });
 
 function start() {
-//Ask the customer what they'd like to do
+    //Ask the customer what they'd like to do
     inquirer.prompt([
         {
             name: "action",
@@ -27,7 +28,7 @@ function start() {
             choices: ["View items for sale", "Leave the store"],
             message: "What would you like to do?"
         }
-    ]).then(function(action) {
+    ]).then(function (action) {
         if (action.action === "View items for sale") {
             viewItems();
         } else if (action.action === "Leave the store") {
@@ -35,29 +36,83 @@ function start() {
         }
     });
 }
-//   function start() {
-//     connection.query("SELECT * FROM Products", function(err, res){
-//         if(err) throw err;
+// View items function
+function viewItems() {
+    var query = "SELECT * FROM products";
+    connection.query(query, function (err, results) {
+        if (err) throw err;
+        // build the table 
+        consoleTable(results);
+        inquirer.prompt([
+            {
+                name: "id",
+                message: "Enter the ID of the product you want to purchase",
+                validate: function (value) {
+                    if (value > 0 && isNaN(value) === false && value <= results.length) {
+                        return true;
+                    }
+                    return false;
+                }
+            },
+            {
+                name: "quanity",
+                message: "How many would you like to buy?",
+                validate: function (value) {
+                    if (value > 0 && isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+        ]).then(function (transaction) {
+            var itemQuanity;
+            var itemPrice;
+            var itemName;
+            // var departmentName;
 
-        
-//         console.log("----------------------------------------------------------------------------------------------------")
-  
-//         for( var i = 0; i < res.lenght; i++) {
-//             console.log("Id " + res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quanity);
-//             console.log("----------------------------------------------------------------------------------------------------")
-//         }
+            for (var i = 0; i < results.length; i++) {
+                if (parseInt(transaction.id) === results[i].item_id) {
+                    itemQuanity = results[i].stock_quanity;
+                    itemPrice = results[i].price;
+                    itemName = results[i].product_name;
+                    // departmentName = results[i].department_name;
+                }
+            }
+            //If insuffcient stock 
+            if (parseInt(transaction.qty) > itemQuanity) {
+                console.log("\nInsufficient inventory for your requested quantity. We have "
+                    + itemQuanity + " in stock. Try again.\n");
+                stock();
+            } else if (parseInt(transaction.qty) <= itemQuanity) {
+                console.log("\nCongrats! You successfully purchased " + transaction.qty
+                    + " of " + itemName + ".");
+                lowerQty(transaction.id, transaction.qty, itemQuanity, itemPrice);
+            }
+            })
+    })
+}
 
-//         console.log(" ");
-//         inquirer.prompt([
-//             {
-//                 type: "input",
-//                 name: "id",
-//                 message: "What is the id of the prodduct that you'd like to purchase?"
-//             }
-//         ])
+    function consoleTable(results) {
+        // create empty values array
+        var values = [];
+        // loop through all results
+        for (var i = 0; i < results.length; i++) {
+            // create resultObject for each iteration. properties of object will be column
+            // headings in the console table
+            var resultObject = {
+                ID: results[i].item_id,
+                Item: results[i].product_name,
+                Price: "$" + results[i].price,
+                Department: results[i].department_name
+            };
+            // push result object to values array
+            values.push(resultObject);
+        }
+        // create table with title items for sale with the values array
+        console.table("\nItems for Sale", values);
+    }
 
-       
-//     })
+});
+}
 
-// }
-// start();
+
