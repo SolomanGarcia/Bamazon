4ce818605e9dbd20af9663cc888c18503c010a09
Soloman Garcia <soloman.garcia@gmail.com>
2019-10-09 17:11:36 -0600

    created sales revenue function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,7 @@
 //Require mysql and inquirer
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var consoleTable = require("console.table");
+var consoleTableNPM = require("console.table");
 var pw = require("./pw.js");
 //Create a connection to mysql
 var connection = mysql.createConnection({
@@ -68,6 +68,7 @@ function viewItems() {
             var itemQuanity;
             var itemPrice;
             var itemName;
+            var productSales;
             // var departmentName;
 
             for (var i = 0; i < results.length; i++) {
@@ -75,7 +76,7 @@ function viewItems() {
                     itemQuanity = results[i].stock_quanity;
                     itemPrice = results[i].price;
                     itemName = results[i].product_name;
-                    // departmentName = results[i].department_name;
+                    productSales = results[i].product_sales;
                 }
             }
             //If insuffcient stock 
@@ -87,11 +88,12 @@ function viewItems() {
                 console.log("\nCongrats! You successfully purchased " + transaction.quanity
                     + " of " + itemName + ".");
                 lowerQuanity(transaction.id, transaction.quanity, itemQuanity, itemPrice);
+                salesRevenue(transaction.id, transaction.qty, productSales, itemPrice);
             }
         })
     })
 
-
+}
 function consoleTable(results) {
     // create empty values array
     var values = [];
@@ -111,7 +113,54 @@ function consoleTable(results) {
     // create table with title items for sale with the values array
     console.table("\nItems for Sale", values);
 }
+function lowerQuanity(item, purchaseQuanity, stockQuanity, price) {
+	// query with an update, set stock equal to stockqty - purchase qty
+	// where the item_id equals the id the user entered
+	connection.query(
+		"UPDATE products SET ? WHERE ?", 
+		[
+			{
+				stock_quanity: stockQuanity - parseInt(purchaseQuanity)
+			},
+			{
+				item_id: parseInt(item)
+			}
+		],
+		// throw error if error, else run displayCost
+		function(err, response) {
+			if (err) throw err;
+	});
 }
+function salesRevenue(item, purchaseQuanity, productSales, price) {
+	var customerCost = parseInt(purchaseQuanity) * price;
+	// query with an update, set product rev equal to current product sales + 
+	// purchase qty * price where the item id equals the id the user entered
+	connection.query(
+		"UPDATE products SET ? WHERE ?", 
+		[
+			{
+				product_sales: productSales + customerCost
+			}, 
+			{
+				item_id: parseInt(item)
+			}
+		], 
+		function(err, response) {
+			if (err) throw err;
+			// log it fixed to 2 decimals to tell customer what their price was
+			console.log("The total price is $" + customerCost.toFixed(2) 
+				+ ". Gotta love no sales tax. Thanks for you purchase!\n");
+			// run welcome function
+			start();
+	});
+}
+
+// exit function says bye to user and ends db connection
+function exit() {
+	console.log("\nThanks for stopping by! Have a good day.");
+	connection.end();
+}
+
 
 
 
--- schema.sql
+++ schema.sql
@@ -10,20 +10,21 @@ product_name VARCHAR (100) NOT NULL,
 department_name VARCHAR (100) NOT NUll,
 price DECIMAL (10,2) NOT NULL,
 stock_quanity INT (50) NOT NUll,
+product_sales DECIMAL(10,2) DEFAULT 0,
 PRIMARY KEY (item_id)
 );
 
-INSERT INTO products (product_name, department_name, price, stock_quanity)
-VALUES ("Rucksack", "Outdoors", 98.95, 11),
-("Crock Pot", "Kitchen", 49.95, 26),
-("Power Drill", "Tools", 84.95, 17),
-("Winter Coat", "Clothing", 109.95, 8),
-("Diamond Necklace", "Jewlery", 4995.95, 3),
-("Tent", "Outdoors", 135.95, 6),
-("Bread Maker", "Kitchen", 89.95, 11),
-("Hammer", "Tools", 29.95, 4),
-("Sandals", "Clothing", 4.95, 17),
-("Gold Bracelet", "Jewlery", 995.95, 6);
+INSERT INTO products (product_name, department_name, price, stock_quanity, product_sales )
+VALUES ("Rucksack", "Outdoors", 98.95, 11, 100),
+("Crock Pot", "Kitchen", 49.95, 26, 145),
+("Power Drill", "Tools", 84.95, 17, 85),
+("Winter Coat", "Clothing", 109.95, 8, 42),
+("Diamond Necklace", "Jewlery", 4995.95, 3, 17),
+("Tent", "Outdoors", 135.95, 6, 253),
+("Bread Maker", "Kitchen", 89.95, 11, 354),
+("Hammer", "Tools", 29.95, 4, 196),
+("Sandals", "Clothing", 4.95, 17, 745),
+("Gold Bracelet", "Jewlery", 995.95, 6, 72);
 
 SELECT * FROM products;
 
