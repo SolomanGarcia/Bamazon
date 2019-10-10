e8b8770d86e9d07932d7b3775e3b48188f48fea9
Soloman Garcia <soloman.garcia@gmail.com>
2019-10-09 19:43:11 -0600

    finished bamazonmanagerjs and debugged

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,9 +1,8 @@
-//Require mysql and inquirer
+
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var consoleTableNPM = require("console.table");
 var pw = require("./pw.js");
-//Create a connection to mysql
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -11,16 +10,14 @@ var connection = mysql.createConnection({
     password: pw.pw,
     database: "bamazon_db"
 });
-//Create a connection to database
+
 connection.connect(function (err) {
     if (err) throw err;
     console.log("\n--------------------Welocome to Bamazon--------------------\n")
-    //Start
     start();
 });
 
 function start() {
-    //Ask the customer what they'd like to do
     inquirer.prompt([
         {
             name: "action",
@@ -36,12 +33,11 @@ function start() {
         }
     });
 }
-// View items function
+
 function viewItems() {
     var query = "SELECT * FROM products";
     connection.query(query, function (err, results) {
-        if (err) throw err;
-        // build the table 
+        if (err) throw err; 
         consoleTable(results);
         inquirer.prompt([
             {
@@ -55,7 +51,7 @@ function viewItems() {
                 }
             },
             {
-                name: "quanity",
+                name: "quantity",
                 message: "How many would you like to buy?",
                 validate: function (value) {
                     if (value > 0 && isNaN(value) === false) {
@@ -65,7 +61,7 @@ function viewItems() {
                 }
             }
         ]).then(function (transaction) {
-            var itemQuanity;
+            var itemQuantity;
             var itemPrice;
             var itemName;
             var productSales;
@@ -73,68 +69,58 @@ function viewItems() {
 
             for (var i = 0; i < results.length; i++) {
                 if (parseInt(transaction.id) === results[i].item_id) {
-                    itemQuanity = results[i].stock_quanity;
+                    itemQuantity = results[i].stock_quantity;
                     itemPrice = results[i].price;
                     itemName = results[i].product_name;
                     productSales = results[i].product_sales;
                 }
             }
-            //If insuffcient stock 
-            if (parseInt(transaction.quanity) > itemQuanity) {
+             
+            if (parseInt(transaction.quantity) > itemQuantity) {
                 console.log("\nInsufficient inventory for your requested quantity. We have "
-                    + itemQuanity + " in stock. Try again.\n");
+                    + itemQuantity + " in stock. Try again.\n");
                 start();
-            } else if (parseInt(transaction.quanity) <= itemQuanity) {
-                console.log("\nCongrats! You successfully purchased " + transaction.quanity
+            } else if (parseInt(transaction.quantity) <= itemQuantity) {
+                console.log("\nCongrats! You successfully purchased " + transaction.quantity
                     + " of " + itemName + ".");
-                lowerQuanity(transaction.id, transaction.quanity, itemQuanity, itemPrice);
-                salesRevenue(transaction.id, transaction.quanity, productSales, itemPrice);
+                lowerQuantity(transaction.id, transaction.quantity, itemQuantity, itemPrice);
+                salesRevenue(transaction.id, transaction.quantity, productSales, itemPrice);
             }
         })
     })
 
 }
 function consoleTable(results) {
-    // create empty values array
     var values = [];
-    // loop through all results
     for (var i = 0; i < results.length; i++) {
-        // create resultObject for each iteration. properties of object will be column
-        // headings in the console table
         var resultObject = {
             ID: results[i].item_id,
             Item: results[i].product_name,
             Price: "$" + results[i].price,
             Department: results[i].department_name
         };
-        // push result object to values array
         values.push(resultObject);
     }
-    // create table with title items for sale with the values array
     console.table("\nItems for Sale", values);
 }
-function lowerQuanity(item, purchaseQuanity, stockQuanity, price) {
-    // query with an update, set stock equal to stockqty - purchase qty
-    // where the item_id equals the id the user entered
+function lowerQuantity(item, purchaseQuantity, stockQuantity, price) {
     connection.query(
         "UPDATE products SET ? WHERE ?",
         [
             {
-                stock_quanity: stockQuanity - parseInt(purchaseQuanity)
+                stock_quantity: stockQuantity - parseInt(purchaseQuantity)
             },
             {
                 item_id: parseInt(item)
             }
         ],
-        // throw error if error, else run displayCost
+        
         function (err, response) {
             if (err) throw err;
         });
 }
-function salesRevenue(item, purchaseQuanity, productSales, price) {
-    var customerCost = parseInt(purchaseQuanity) * price;
-    // query with an update, set product rev equal to current product sales + 
-    // purchase qty * price where the item id equals the id the user entered
+function salesRevenue(item, purchaseQuantity, productSales, price) {
+    var customerCost = parseInt(purchaseQuantity) * price;
     connection.query(
         "UPDATE products SET ? WHERE ?",
         [
@@ -147,15 +133,12 @@ function salesRevenue(item, purchaseQuanity, productSales, price) {
         ],
         function (err, response) {
             if (err) throw err;
-            // log it fixed to 2 decimals to tell customer what their price was
             console.log("The total price is $" + customerCost.toFixed(2)
                 + ". Thanks for you purchase!\n");
-            // run welcome function
             start();
         });
 }
 
-// exit function says bye to user and ends db connection
 function exit() {
     console.log("\nThanks for stopping by! Have a good day.");
     connection.end();
--- bamazonManager.js
+++ bamazonManager.js
@@ -51,3 +51,150 @@ function viewProducts() {
 		start();
 	});
 }
+
+function viewLowInventory() {
+	var query = "SELECT * FROM products WHERE stock_quantity<5";
+	connection.query(query, function(err, results) {
+		if (err) throw err;
+		consoleTable("\nLow Product Inventory Data", results);
+		start();
+	});
+}
+
+function addToInventory() {
+	connection.query("SELECT * FROM products", function (err, results) {
+		if (err) throw err;
+		consoleTable("\nCurrent Inventory Data", results);
+		inquirer.prompt([
+			{
+				name: "id",
+				message: "Input the item ID to increase inventory on.",
+				validate: function(value) {
+					if (isNaN(value) === false && value > 0 && value <= results.length) {
+						return true;
+					}
+					return false;
+				}
+			},
+			{
+				name: "amount",
+				message: "Input the amount to increase inventory by.",
+				validate: function(value) {
+					if (isNaN(value) === false && value > 0) {
+						return true;
+					}
+					return false;
+				}
+			}
+		]).then(function(answer) {
+			var itemQuantity;
+			 for (var i = 0; i < results.length; i++) {
+				if (parseInt(answer.id) === results[i].item_id) {
+					itemQuantity = results[i].stock_quantity;
+				}
+			}
+			increaseQuantity(answer.id, itemQuantity, answer.amount);
+		});
+	});
+}
+
+function increaseQuantity(item, stockQuantity, addQuantity) {
+	connection.query(
+		"UPDATE products SET ? WHERE ?", 
+		[
+			{
+				stock_quantity: stockQuantity + parseInt(addQuantity)
+			}, 
+			{
+				item_id: parseInt(item)
+			}
+		], 
+		function(err, results) {
+			if (err) throw err;
+			console.log("\nInventory successfully increased.\n");
+			start();
+	});
+}
+
+function addNewProduct() {
+	connection.query("SELECT * FROM departments", function (err, results) {
+		inquirer.prompt([
+			{
+				name: "item",
+				message: "Input new item to add."
+			},
+			{
+				name: "department",
+				type: "list",
+				choices: function() {
+					var deptArray = [];
+					for (var i = 0; i < results.length; i++) {
+						deptArray.push(results[i].department_name);
+					}
+					
+					return deptArray;
+				},
+				message: "Which department does this item belong in?"
+			},
+			{
+				name: "price",
+				message: "How much does this item cost?",
+				validate: function(value) {
+					if (value >= 0 && isNaN(value) === false) {
+						return true;
+					}
+					return false;
+				}
+			},
+			{
+				name: "inventory",
+				message: "How much inventory do we have?",
+				validate: function(value) {
+					if (value > 0 && isNaN(value) === false) {
+						return true;
+					}
+					return false;
+				}			
+			}
+		]).then(function(newItem) {
+			addItemToDb(newItem.item, newItem.department, 
+				parseFloat(newItem.price).toFixed(2), parseInt(newItem.inventory));
+		});
+	});
+}
+
+function addItemToDb(item, department, price, quantity) {
+	connection.query(
+		"INSERT INTO products SET ?", 
+		{
+			product_name: item,
+			department_name: department,
+			price: price,
+			stock_quantity: quantity
+		},
+		function(err, results) {
+			if (err) throw err;
+			console.log("\nNew product successfully added.\n");
+			start();
+	});
+}
+
+
+function consoleTable(title, results) {
+	var values = [];
+	for (var i = 0; i < results.length; i++) {
+		var resultObject = {
+			ID: results[i].item_id,
+			Item: results[i].product_name,
+			Price: "$" + results[i].price,
+			Inventory: results[i].stock_quantity + " units"
+		};
+		values.push(resultObject);
+	}
+	console.table(title, values);
+}
+
+function exit() {
+	console.log("\nGet back to work!.");
+	connection.end();
+}
